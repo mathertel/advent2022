66a97e10298c38799166a44bacb08a46fc32279d
Matthias Hertel <mathertel@hotmail.com>
2022-12-01 23:01:15 +0100

    day 02

--- advent2022.js
+++ advent2022.js
@@ -1,1 +1,13 @@
 // some global included code for the Advent Calendar 2022
+
+function codeCopy(evt) {
+  const t = evt.target;
+  const box = t.getBoundingClientRect();
+  if ((evt.clientX > box.right - 32) && (evt.clientY < box.top + 32)) {
+    console.log(t.innerText);
+    navigator.clipboard.writeText(t.innerText);
+  }
+}
+document.querySelectorAll('pre>code').forEach(e => {
+  e.addEventListener('click', codeCopy);
+});
