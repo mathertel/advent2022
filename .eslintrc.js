2dd56b6ab0f9473c2ea240adec8a25e934d3e90a
Matthias Hertel <mathertel@hotmail.com>
2022-12-10 14:44:55 +0100

    day11

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: 'standard-with-typescript',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['html'],
+  rules: {
+    semi: [2, 'always'],
+    'no-multiple-empty-lines': ['error', { max: 2, maxEOF: 1 }],
+    'space-before-function-paren': ['warn', 'never'],
+    'no-unused-vars': ['warn', { ignoreRestSiblings: true, argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    'no-unused-expressions': 0
+  }
+};
