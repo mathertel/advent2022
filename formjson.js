2329949309d6cc0b27025680e4b6d394a012d2b8
Matthias Hertel <mathertel@hotmail.com>
2022-12-15 12:22:28 +0100

    day 15

--- formjson.js
+++ formjson.js
@@ -2,42 +2,43 @@
 
 class FormJsonData extends HTMLFormElement {
   // private
-  analyzed = false;
-  emptyRecord = {};
-  booleanAttributes = new Set();
+  #analyzed = false;
+  #emptyRecord = {};
+  #booleanAttributes = new Set();
 
   // connectedCallback() {
   //   ... is called when the html element is ready, but form elements may not be there at this time.
-  //   console.log("conn");
+  //   console.log('forjson:', 'connected');
   //   document.addEventListener('DOMContentLoaded', this.analyze.bind(this));
   // }
 
   // analyse all form element types that can be used as input to create an empty record
   // including all known form elements.
   analyze() {
-    this.querySelectorAll('input[name],textarea[name]').forEach(e => this.emptyRecord[e.name] = '');
-    this.querySelectorAll('select[name]').forEach(e => this.emptyRecord[e.name] = e.value || '');
-    this.querySelectorAll('input[name][type=range]').forEach(e => this.emptyRecord[e.name] = 0);
-    this.querySelectorAll('input[name][type=color]').forEach(e => this.emptyRecord[e.name] = '#000000');
+    this.querySelectorAll('input[name],textarea[name]').forEach(e => this.#emptyRecord[e.name] = '');
+    this.querySelectorAll('select[name]').forEach(e => this.#emptyRecord[e.name] = e.value || '');
+    this.querySelectorAll('input[name][type=range]').forEach(e => this.#emptyRecord[e.name] = 0);
+    this.querySelectorAll('input[name][type=color]').forEach(e => this.#emptyRecord[e.name] = '#000000');
     this.querySelectorAll('input[name][type=checkbox]').forEach(e => {
-      this.emptyRecord[e.name] = false;
-      this.booleanAttributes.add(e.name);
+      this.#emptyRecord[e.name] = false;
+      this.#booleanAttributes.add(e.name);
     });
-    this.analyzed = true;
-    console.log(this.emptyRecord);
+    this.#analyzed = true;
+
+    console.log('forjson:', 'emptyRecord:', this.#emptyRecord);
     // not: output, meter
   }
 
 
   // return the formData as Object including empty values.
   getJsonData() {
-    if (!this.analyzed) this.analyze();
+    if (!this.#analyzed) this.analyze();
     const formData = new FormData(this);
     let jData = Object.fromEntries(formData);
-    jData = Object.assign({}, this.emptyRecord, jData);
+    jData = Object.assign({}, this.#emptyRecord, jData);
 
     Object.entries(jData).forEach(([name, value]) => {
-      if (this.booleanAttributes.has(name)) {
+      if (this.#booleanAttributes.has(name)) {
         jData[name] = Boolean(value === 'on');
       }
     });
@@ -47,18 +48,37 @@ class FormJsonData extends HTMLFormElement {
 
   // set the value of the form elements according the name including form output elements
   setJsonData(jData) {
-    if (!this.analyzed) this.analyze();
+    let hasChanged = false;
+    if (!this.#analyzed) this.analyze();
     Object.entries(jData).forEach(([name, value]) => {
       this.querySelectorAll(`*[name=${name}]`).forEach(el => {
         if (el.type === 'radio') {
-          el.checked = (el.value === value);
+          if (el.checked !== (el.value === value)) {
+            el.checked = (el.value === value);
+            hasChanged = true;
+          }
         } else if (el.type === 'checkbox') {
-          el.checked = (!!value);
-        } else {
+          if (el.checked !== (!!value)) {
+            el.checked = (!!value);
+            hasChanged = true;
+          }
+        } else if ((el.tagName === 'METER') || (el.tagName === 'OUTPUT')) {
+          // output elements
           el.value = value;
+        } else {
+          if (el.value !== value) {
+            el.value = value;
+            hasChanged = true;
+          }
         }
       });
     });
+
+    if (hasChanged) {
+      // create 'change' event on form element when form data was really changed.
+      const evt = new Event('change');
+      this.dispatchEvent(evt);
+    }
   } // setJsonData()
 
 } // class
